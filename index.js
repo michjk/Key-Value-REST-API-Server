0ce99f0ef7e174c023f14f865579310201e1438d
michjk <michaelj.kosasih@gmail.com>
2018-05-30 19:45:15 +0800

    debug NaN

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const port = process.env.PORT || 5656;
 
 app.use(bodyParser.json());
 app.use('/object', objectRouter);
-app.use('/trial', objectRouter);
 app.use((err, req, res, next) => {
     res.status(404).json({'error': err.message});
 });
