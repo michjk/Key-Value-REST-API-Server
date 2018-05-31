1e3d140c8132dea647a7c4a1ba9772101e3434aa
michjk <michaelj.kosasih@gmail.com>
2018-05-31 15:42:55 +0800

    update test suite

--- test/get.test.js
+++ test/get.test.js
@@ -4,18 +4,33 @@ const mongoose = require('../mongoose');
 const app = require('../app');
 const ObjectModel = require('../models/object');
 
-describe('GET /object/:key', () => {
+describe('GET request host/object/:key', () => {
+  let objectList = []
+  beforeAll(async()=> {
+    await mongoose.connect(process.env.MONGODB_URI);
+    
+    await ObjectModel.remove({});
+    objectList.push(await ObjectModel.create({key: "key", value: "1"}));
+    objectList.push(await ObjectModel.create({key: "key", value: "2"}));
+    objectList.push(await ObjectModel.create({key: "key", value: "3"}));
+    objectList.push(await ObjectModel.create({key: "key", value: "4"}));
+    
+    await mongoose.disconnect(process.env.MONGODB_URI);
+  })
   beforeEach(async() => {
     await mongoose.connect(process.env.MONGODB_URI);
-    await ObjectModel.remove({});
-    await ObjectModel.create({key: "key", value: "value"});
   });
   afterEach(async() => {
     await mongoose.disconnect();
   });
-  test('should accept a key and return the latest value', async() => {
+  test('GET request accept a key and return the latest value', async() => {
     const response = await request(app).get('/object/key');
     expect(response.status).toBe(200);
-    expect(response.body.value).toBe('value');
+    expect(response.body.value).toBe('4');
+  });
+  test('GET request accept a key and return the third value by using timestamp', async() => {
+    const response = await request(app).get(`/object/key?timestamp=${objectList[2].timestamp.getTime()}`);
+    expect(response.status).toBe(200);
+    expect(response.body.value).toBe('3');
   });
 });
