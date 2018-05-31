f328c2bb021357e2c60f788ee9dbfe17711d5db2
michjk <michaelj.kosasih@gmail.com>
2018-05-31 17:01:18 +0800

    update test set

--- test/get.test.js
+++ test/get.test.js
@@ -14,6 +14,7 @@ describe('Route GET /object/:key', () => {
     objectList.push(await ObjectModel.create({key: "key", value: "2"}));
     objectList.push(await ObjectModel.create({key: "key", value: "3"}));
     objectList.push(await ObjectModel.create({key: "key", value: "4"}));
+    objectList.push(await ObjectModel.create({key: "key2", value: "11"}));
     
     await mongoose.disconnect(process.env.MONGODB_URI);
   })
@@ -26,22 +27,41 @@ describe('Route GET /object/:key', () => {
   test('GET request accept a key and return the latest value', async() => {
     const response = await request(app).get('/object/key');
     expect(response.status).toBe(200);
-    expect(response.body.value).toBe('4');
+    expect(response.body.value).toBe(objectList[3].key);
+  });
+  test('GET request accept a key2 and return the latest value', async() => {
+    const response = await request(app).get('/object/key2');
+    expect(response.status).toBe(200);
+    expect(response.body.value).toBe(objectList[4].key);
   });
   test('GET request accept a key & timestamp and return the third value', async() => {
     const response = await request(app).get(`/object/key?timestamp=${objectList[2].timestamp.getTime()}`);
     expect(response.status).toBe(200);
-    expect(response.body.value).toBe('3');
+    expect(response.body.value).toBe(objectList[2].key);
   });
-  test('GET request accept a key & (third value timestamp-1) and return the second value', async() => {
+  test('GET request accept a key & timestamp and return the second value', async() => {
     const response = await request(app).get(`/object/key?timestamp=${objectList[2].timestamp.getTime()-1}`);
     expect(response.status).toBe(200);
-    expect(response.body.value).toBe('2');
+    expect(response.body.value).toBe(objectList[1].key);
   });
   test('GET request accept a key & timestamp before first value and return error', async() => {
-    const response = await request(app).get(`/object/key?timestamp=1000`);
+    const response = await request(app).get('/object/key?timestamp=1000');
     expect(response.status).toBe(404);
-    console.log(response.body);
     expect(response.body.error).toBe("Value of key is not found!");
+  });
+  test('GET request accept an unknown key and return error', async() => {
+    const response = await request(app).get('/object/key3');
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBe("Value of key is not found!");
+  });
+  test('GET request accept a key & empty timestamp format and return error', async() => {
+    const response = await request(app).get('/object/key?timestamp=');
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBe('Incorrect timestamp value!');
+  });
+  test('GET request accept an unknown key and return error', async() => {
+    const response = await request(app).get(`/object/key?timestamp=sadasd`);
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBe('Incorrect timestamp value!');
   });
 });
