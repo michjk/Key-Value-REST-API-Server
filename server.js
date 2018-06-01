6adb767ca6f1265729f9720d5fd8e1dc573b7604
Michael JK <michaelj.kosasih@gmail.com>
2018-06-01 19:14:53 +0800

    debug port number

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require('./mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 
 const app = require('./app');
+console.log(process.env.PORT);
 const port = process.env.PORT || 5656;
 app.listen(port, () => {
     console.log(`Listening at ${port}`);
