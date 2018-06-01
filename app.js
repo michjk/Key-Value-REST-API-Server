d67e39b58fcfe83e7c3d09aeeb91d7b1d830dd0c
michjk <michaelj.kosasih@gmail.com>
2018-06-01 20:50:36 +0800

    add librato

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const objectRouter = require('./routers/objectRouter');
 
 const app = express();
 
+if (process.env.NODE_ENV == 'production')
+    const librato = require('librato-node');
+    app.use(librato.middleware());
+console.log(process.env.NODE_ENV);
 app.use(bodyParser.json());
 app.use('/object', objectRouter);
 app.use((err, req, res, next) => {
